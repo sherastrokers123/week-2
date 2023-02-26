76e801724b19026b7f00a308b64664e094928000
Cessabit <sherastrokers123@yandex.ru>
2023-02-26 23:54:13 +0300

    upd

---
+++ 2-todolist /src/Todolist.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+type PropsType = {
+    title: string
+    tasks: Array<TaskType>
+}
+
+export function Todolist(props: PropsType) {
+    return <div>
+        <h3>{props.title}</h3>
+        <div>
+            <input />
+            <button>+</button>
+        </div>
+        <ul>
+            {props.tasks.map((task: TaskType) => {
+                return (
+                    <li key={task.id}>
+                        <input type="checkbox" checked={task.isDone} />
+                        <span>{task.title}</span>
+                    </li>
+                )
+            })}
+
+
+            <li>
+                <input type="checkbox" checked={props.tasks[0].isDone} />
+                <span>{props.tasks[0].title}</span>
+            </li>
+
+        </ul>
+        <div>
+            <button>Al</button>
+            <button>Active</button>
+            <button>Completed</button>
+        </div>
+    </div>
+}
